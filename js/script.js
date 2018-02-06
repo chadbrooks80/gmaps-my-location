9605967d63ceee325238cf57d33a00945fdf1d41
Chad Brooks <chadbrooks80@gmail.com>
2018-02-06 12:21:09 +0800

    now have everything set up where if you click the marker, it shows the info window with the Place Name

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 //initialize the map:
 var map;
 //used to store all the places after making a json request
-var places = [];
-//used to store marker objects placed on map. 
-var markers = []
-//Array for creating markers of my favorite locations:
-var favLocations = [
-
-];
+var places = []; 
+var markers = [];
+var bounds;
 
 //record placeIDs used to get coordinates and other information.
 var placeIDs = [
@@ -20,40 +16,54 @@ var placeIDs = [
     "ChIJj514rSCBa4cRn1UBXj-8nqU",
     "ChIJ281BYiGBa4cRicArfgS5kGI",
     "ChIJ4c_pIWiEa4cRzXmDg9Oi9XI",
-    "ChIJk4E8CwB_a4cRQrSQeNwQmTA",
-    "ChIJZ2iOftloQocRdcCuYDevo0o",
-    "ChIJux94CcN4bIcRcH7lFkSAUfo",
-    "ChIJkeVs4AWHbIcRj4KAN2u2O9U",
-    "ChIJIwwzg8R4bIcR0k02y28L3is",
-    "ChIJyc8brt-Aa4cRXGBd6TXqG1U",
-    "ChIJ2_JElnSEa4cRoNi8ffasMQ8",
-    "ChIJTwxKHKuBa4cRhDgjbDJSXMQ",
-    "ChIJkahrxs2BbIcRSPKtTkM2VCM",
-    "ChIJRV4I2IyAa4cRP4O3lptbl_Y",
-    "ChIJAexs4PiIa4cRYHRp5qqymJQ"
+    "ChIJk4E8CwB_a4cRQrSQeNwQmTA"
+    // "ChIJZ2iOftloQocRdcCuYDevo0o",
+    // "ChIJux94CcN4bIcRcH7lFkSAUfo",
+    // "ChIJkeVs4AWHbIcRj4KAN2u2O9U",
+    // "ChIJIwwzg8R4bIcR0k02y28L3is",
+    // "ChIJyc8brt-Aa4cRXGBd6TXqG1U",
+    // "ChIJ2_JElnSEa4cRoNi8ffasMQ8",
+    // "ChIJTwxKHKuBa4cRhDgjbDJSXMQ",
+    // "ChIJkahrxs2BbIcRSPKtTkM2VCM",
+    // "ChIJRV4I2IyAa4cRP4O3lptbl_Y",
+    // "ChIJAexs4PiIa4cRYHRp5qqymJQ"
 ];
 
 function initMap() {
     // initialize map and place into the div with id of map
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 39.7047, lng: -105.0814 },
-        zoom: 9
+        center: { lat: 39.6813, lng: -105.0559   },
+        zoom: 11
     });
 
-
-    // set up infoWindow & service for Google Places
-    infoWindow = new google.maps.InfoWindow();
-    service = new google.maps.places.PlacesService(map)
+    // set up infoWindow, bounds & service for Google Places
+    var service = new google.maps.places.PlacesService(map);
+    bounds = new google.maps.LatLngBounds();
+    var largeInfowindow = new google.maps.InfoWindow();
 
     // Goes through Places by ID and get's location
     for(i=0; i < placeIDs.length; i++) {
         service.getDetails({placeId: placeIDs[i]},
             function(place, status) {
-                if(status == google.maps.places.PlacesServiceStatus.OK) {
-                    places.push(place);
-                    addMarker(place);
-                } else {
+                if(status != google.maps.places.PlacesServiceStatus.OK) {
                     console.log("error: " + status);
+                } else {
+                    marker = new google.maps.Marker({
+                        map: map,
+                        location: place.geometry.location,
+                        title: place.name
+                    });
+                    
+                    marker.setPosition(place.geometry.location);
+                    //addMarker(place, infoWindow);
+                    
+                    marker.placeDetails = place;
+                    
+                    markers.push(marker);
+
+                    marker.addListener('click', function() {
+                            populateInfoWindow(this, largeInfowindow)
+                    });
                 }
             }
         );
@@ -74,7 +84,8 @@ function initMap() {
         marker = new google.maps.Marker({
             map: map,
             location: place.geometry.location,
-            title: "test"
+            title: "test",
+            animation: google.maps.Animation.DROP,
         });
         markers.push(marker);
         marker.setPosition(place.geometry.location);
@@ -87,26 +98,16 @@ function initMap() {
 
 }
 
-function addMarker(place) {
-    marker = new google.maps.Marker({
-        map: map,
-        location: place.geometry.location,
-        title: "test title"
-    });
-    marker.setPosition(place.geometry.location);
-    markers.push(marker);
-    console.log(marker);
-    
 
-}
-
-
-
-function populateInfoWindow(marker, markerInfoWindow) {
-    markerInfoWindow.marker = marker;
-    markerInfoWindow.setContent('test me out!');
-    markerInfoWindow.open(map, marker);
-    infoWindow.addListener('closeclick', function() {
-        markerInfoWindow.setMarker = null;
-    });
+function populateInfoWindow(marker, infoWindow) {
+    //add infowindow
+    if (infoWindow.marker != marker) {
+        infoWindow.marker = marker;
+        infoWindow.setContent("<div>" + marker.placeDetails.name + "</div>");
+        infoWindow.open(map, marker);
+        //ensure marker is closed when x is clicked
+        infoWindow.addListener('closeclick', function() {
+            infoWindow.setMarker = null;
+        });
+    }
 }
